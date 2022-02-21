478c9ef18c58a44a056f683e3346be17bffeb282
MiroMiroOnTheWall <100101556+MiroMiroOnTheWall@users.noreply.github.com>
2022-02-20 17:33:06 -0700

    Create i2c_mux.py

---
+++ examples/i2c_mux.py
@@ -0,0 +1,29 @@
+# intended for import to change channel of TCA9548A's
+# import i2c_mux then call i2c_mux.set_chan(multiplexer_addr,multiplexer_channel)
+# example call i2c_mux.set_chan(i2c_mux.RIGHT_MUX,7) to set multiplexer 0x70 to channel 7
+
+import smbus
+import time
+import sys
+
+channel_array=[0b00000001,0b00000010,0b00000100,0b00001000,0b00010000,0b00100000,0b01000000,0b10000000]
+
+# Address of the two mux drivers
+RIGHT_MUX = 0x70
+LEFT_MUX = 0x71
+
+def set_chan(multiplexer,i2c_channel_setup):
+    bus = smbus.SMBus(1)
+
+    # turn the other mux off by setting to unused channel 0
+    if multiplexer == RIGHT_MUX:
+        bus.write_byte(LEFT_MUX,channel_array[0])
+    else:
+        bus.write_byte(RIGHT_MUX,channel_array[0])
+
+    # turn the requested mux channel on
+    bus.write_byte(multiplexer,channel_array[i2c_channel_setup])
+
+    # Sleep a millisecond and read the mux status (comment out after testing)
+    time.sleep(0.001)
+    print("I2C channel status:", bin(bus.read_byte(multiplexer)))
